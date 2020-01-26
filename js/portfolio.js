d00c1d856388040470043d969ee3acefa7cb63d7
Aidan <aidnolan@hotmail.com>
2020-01-26 23:55:51 +0000

    Update profile pic and intro

--- js/portfolio.js
+++ js/portfolio.js
@@ -27,27 +27,22 @@
 	// Instructions to browser re: size reduction in classes/IDs specified
 	function widthView(){
 		var viewerWidth = $(window).width();
+		console.log(viewerWidth);
 		if(viewerWidth < 768){
 			$(".profiles span").css("display", "none");
-			$("#formbox .profiles").css("textAlign","center");
 			$(".navbar-nav a").css("width", "100%");
+			$("#formbox .profiles").css("text-align","center");
 			$("#myForm").insertBefore($("#mySocial"));
 			$("#mySocial").css("padding","20px 0 20px 0");
-			$("#coursesUndertaken").css("font-size", "16px");
-			$("#coursebox").css("padding", "10px 20px");
 			$("#introbox #introPic").css("width", "45%");
-			$("#titlebox").css("background-attachment", "local");
 			$(".certificates").css("width", "250px");
-
+		
 		} else {
 			$(".profiles span").css("display", "inline");
-			$("#formbox .profiles").css("textAlign","left");
+			$("#formbox .profiles").css("text-align","left");
 			$(".navbar-nav a").css("width", "100px");
 			$("#mySocial").insertBefore($("#myForm"));
-			$("#coursesUndertaken").css("font-size", "25px");
-			$("#coursebox").css("padding", "30px 150px");
 			$("#introbox #introPic").css("width", "30%");
-			$("#titlebox").css("background-attachment", "fixed");
 			$(".certificates").css("width", "350px");
 		}
 	}
